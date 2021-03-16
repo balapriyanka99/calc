3fc8ae5fd367ca221aabf7326b299947493d0cb6
balapriyanka99 <balapriyankaboddapati@gmail.com>
2021-03-17 01:14:44 +0530

    Code changes done

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,52 +7,52 @@ import java.util.Scanner;
 public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator cal = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        double number1, number2;
+        double val1, val2;
         do {
-            System.out.println("Calculator-DevOps, Choose to perform operation");
-            System.out.print("Press 1 to Square Root\nPress 2 to Factorial\nPress 3 to Log (base e)\nPress 4 to Power\n" +
-                    "Press any other key to exit\nEnter your choice: ");
-            int choice;
+            System.out.println("Calculator using Dev-Ops, select the operation which you wnat to perform ");
+            System.out.print("select 1 for Square Root\nselect 2 for Factorial\nselect 3 to Log(base e)\nselect 4 for Power\n" +
+                    "select any other key to exit\nEnter your choice: ");
+            int ch;
             try {
-                choice = scanner.nextInt();
+                ch = scanner.nextInt();
             } catch (InputMismatchException error) {
                 return;
             }
             try {
 
 
-                switch (choice) {
+                switch (ch) {
                     case 1:
 
                         System.out.print("Enter the number : ");
-                        number1 = scanner.nextDouble();
-                        System.out.println("Square Root result is : " + calculator.squareRoot(number1));
+                        val1 = scanner.nextDouble();
+                        System.out.println("Square Root of the number " + val1 +"is:"+cal.squareRoot(val1));
                         break;
                     case 2:
                         System.out.print("Enter the number : ");
-                        number1 = scanner.nextDouble();
-                        System.out.println("Factorial result is : " + calculator.factorial(number1));
+                        val1 = scanner.nextDouble();
+                        System.out.println("Factorial of the number " + val1 +"is:"+ cal.factorial(val1));
                         break;
                     case 3:
                         System.out.print("Enter the number : ");
-                        number1 = scanner.nextDouble();
-                        System.out.println("Log result is : " + calculator.log(number1));
+                        val1 = scanner.nextDouble();
+                        System.out.println("Log of the number " + val1 +"is:"+ cal.log(val1));
                         break;
                     case 4:
                         System.out.print("Enter the first number : ");
-                        number1 = scanner.nextDouble();
+                        val1 = scanner.nextDouble();
                         System.out.print("Enter the second number : ");
-                        number2 = scanner.nextDouble();
-                        System.out.println("Power result is : " + calculator.power(number1, number2));
+                        val2 = scanner.nextDouble();
+                        System.out.println("Power Answer is : " + cal.power(val1, val2));
                         break;
                     default:
-                        System.out.println("Exiting....");
+                        System.out.println("Exit");
                         return;
                 }
             } catch (InputMismatchException error) {
-                logger.error("Invalid input, Entered input is not a number");
+                logger.error("Invalid input, Entered value is not a number");
                 return;
             }
         } while (true);
@@ -60,38 +60,38 @@ public class Calculator {
     }
 
 
-    public double squareRoot(double number1) {
-        logger.info("[SQUARE ROOT] - " + number1);
-        double result = Math.sqrt(number1);
+    public double squareRoot(double val1) {
+        logger.info("[SQUARE ROOT] - " + val1);
+        double result = Math.sqrt(val1);
         logger.info("[RESULT - SQUARE ROOT] - " + result);
         return result;
     }
 
-    public int factorial(double number1) {
-        logger.info("[FACTORIAL] - " + number1);
-        int result = 1;
-        if(number1 == 1)
+    public int factorial(double val1) {
+        logger.info("[FACTORIAL] - " + val1);
+        int r= 1;
+        if(val1 == 1)
             return 1;
-        for(int i=2; i<=number1; i++){
-            result*=i;
+        for(int i=2; i<=val1; i++){
+            r*=i;
         }
-        logger.info("[RESULT - FACTORIAL] - " + result);
-        return result;
+        logger.info("[RESULT - FACTORIAL] - " + r);
+        return r;
     }
 
 
-    public double log(double number1) {
-        logger.info("[LOG] - " + number1);
-        double result = Math.log(number1);
-        logger.info("[RESULT - LOG] - " + result);
-        return result;
+    public double log(double val1) {
+        logger.info("[LOG] - " + val1);
+        double r = Math.log(val1);
+        logger.info("[RESULT - LOG] - " + r);
+        return r;
     }
 
-    public double power(double number1, double number2) {
-        logger.info("[POWER] - " + number1 +","+number2);
-        double result = Math.pow(number1,number2);
-        logger.info("[RESULT - POWER] - " + result);
-        return result;
+    public double power(double val1, double val2) {
+        logger.info("[POWER] - " + val1 +","+val2);
+        double r = Math.pow(val1,val2);
+        logger.info("[RESULT - POWER] - " + r);
+        return r;
     }
 
 }
